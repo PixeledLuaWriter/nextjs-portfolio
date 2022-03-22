b55120486f7aafb98a1013ede7329af76984cd84
Godcat567 <78665021+PixeledLuaWriter@users.noreply.github.com>
2022-03-22 11:44:24 -0400

    Update URLCard.js

--- components/card/URLCard.js
+++ components/card/URLCard.js
@@ -1,9 +1,11 @@
-
 import React from "react";
-import styles from "../../styles/Home.module.css"
 
-export default function URLCard({ props, {label: {text}} }) => {
-return (
-<a {...props}>{text}</a>
-)
-}
+function URLCard({ props, {label: {text}} }) => {
+   return (
+       <a {...props}>
+        {text}
+       </a>
+   );
+};
+
+export default URLCard
